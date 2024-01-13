73b83e5b0660abbf56d233de8f8e15acf4a45d80
farhan71 <farhanitnstu@gmail.com>
2024-01-14 01:25:39 +0600

    Part 3 done

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,43 +2,60 @@ import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import SingleCard from "../SingleCard/SingleCard";
 
-const Cards = ({dataFileName, stageName, flagColor}) => {
+const Cards = ({ dataFileName, stageName, flagColor }) => {
   const [cards, setCards] = useState([]);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
   const styles = {
-    borderRadius: "50% / 100% 100% 0 0", width:"24px", height:"12px", background:flagColor, transform:"rotate(-90deg)"
-  }
-  useEffect(()=> {
+    borderRadius: "50% / 100% 100% 0 0",
+    width: "24px",
+    height: "12px",
+    background: flagColor,
+    transform: "rotate(-90deg)",
+  };
+  useEffect(() => {
     fetch(`${dataFileName}.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      setCards(data);
-    });
-  },[dataFileName])
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data);
+      });
+  }, [dataFileName]);
+  useEffect(() => {
+    fetch("http://localhost:5000/getfiles", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUploadedFiles(data);
+        console.log(uploadedFiles.length);
+      });
+  }, []);
 
   return (
     <div className="bg-[#F2F4F7] m-4 p-4">
       <div className="flex justify-between items-center p-2">
         <div className="flex items-center">
-        {flagColor && <div style={styles}></div>}
-        <h1 className="text-sm font-bold text-gray-800">
-          {stageName}
-        </h1>
+          {flagColor && <div style={styles}></div>}
+          <h1 className="text-sm font-bold text-gray-800">{stageName}</h1>
         </div>
 
         <span className="bg-[#E8EEF3] font-bold px-2 py-0.5 rounded">0</span>
       </div>
-        <div className="max-w-72 max-h-screen overflow-y-scroll">
-          {
-            cards.map(card => <SingleCard key={card.id} card={card}></SingleCard> )
-          }
-        </div>
+      <div className="max-w-72 max-h-screen overflow-y-scroll">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            uploadedFiles={uploadedFiles}
+          ></SingleCard>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 Cards.propTypes = {
   dataFileName: PropTypes.string.isRequired,
   stageName: PropTypes.string.isRequired,
-  flagColor: PropTypes.string
+  flagColor: PropTypes.string,
 };
 
-export default Cards
+export default Cards;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,51 +1,85 @@
 import PropTypes from "prop-types";
 import { useRef, useState } from "react";
 import { IoClose } from "react-icons/io5";
-const Modal = ({onClose, card}) => {
-    const modalRef = useRef();
-    const [files, setFiles] = useState([]);
-    const closeModal = (e) => {
-        if(modalRef.current === e.target) {
-            onClose();
-        }
+const Modal = ({ onClose, card }) => {
+  const modalRef = useRef();
+  const [files, setFiles] = useState([]);
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      onClose();
     }
-    const handleFileChange = (event) => {
-        const selectedFiles = event.target.files;
-        const uniqueNewFiles = Array.from(selectedFiles).filter(
-            (newFile) => !files.some((existingFile) => existingFile.name === newFile.name)
-          );
-        setFiles((prevFiles)=> [...prevFiles, ...Array.from(uniqueNewFiles)]);
-        
-      };
-      const getFileExtension = (fileName) => {
-        return fileName.slice(((fileName.lastIndexOf('.') - 1) >>> 0) + 2);
-      }
-  return (
-    <div ref={modalRef} onClick={closeModal} className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-[1]">
-        <div className="mt-10 flex flex-col gap-5">
-        <button onClick={onClose} className="place-self-end"><IoClose/></button>
-        <div className="bg-white rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4 ">
-            <h1 className="text-3xl font-bold">Card Attachements</h1>
-            <h2>Card ID: {card.id}</h2>
-            <input type="file" multiple onChange={handleFileChange} />
-            <div>
-      {files && <h2>File List</h2>}
-      <ul>
-        {files.map((file, index) => (
-          <li key={index}>
-            <strong>File {index + 1}:</strong> {file.name} (Extension: {getFileExtension(file.name)})
-          </li>
-        ))}
-      </ul>
-    </div>
-        </div>
-
-        </div>
-    </div>
-  )
-}
-Modal.propTypes = {
-    onClose: PropTypes.func.isRequired, 
-    card: PropTypes.object.isRequired
   };
-export default Modal
+  const handleFileChange = (event) => {
+    const selectedFiles = event.target.files;
+    const uniqueNewFiles = Array.from(selectedFiles).filter(
+      (newFile) =>
+        !files.some((existingFile) => existingFile.name === newFile.name)
+    );
+    setFiles((prevFiles) => [...prevFiles, ...Array.from(uniqueNewFiles)]);
+  };
+  const getFileExtension = (fileName) => {
+    return fileName.slice(((fileName.lastIndexOf(".") - 1) >>> 0) + 2);
+  };
+  const handleUpload = () => {
+    files.forEach((file) => {
+      const formData = new FormData();
+      const filename = card.id + "_" + Date.now() + "_" + file.name;
+      formData.append("name", filename);
+      formData.append("file", file);
+      console.log(...formData);
+      fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    });
+    onClose();
+  };
+  return (
+    <div
+      ref={modalRef}
+      onClick={closeModal}
+      className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-[1]"
+    >
+      <div className="mt-10 flex flex-col gap-5">
+        <button onClick={onClose} className="place-self-end">
+          <IoClose />
+        </button>
+        <div className="bg-white rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4 ">
+          <h1 className="text-3xl font-bold">Card Attachements</h1>
+          <h2>Card ID: {card.id}</h2>
+          <input type="file" multiple onChange={handleFileChange} />
+          <div>
+            {files.length !== 0 ? (
+              <div>
+                <h2>File List</h2>
+                <ul>
+                  {files.map((file, index) => (
+                    <li key={index}>
+                      <strong>File {index + 1}:</strong> {file.name} (Extension:{" "}
+                      {getFileExtension(file.name)})
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <></>
+            )}
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleUpload}
+            >
+              Upload Files
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  card: PropTypes.object.isRequired,
+};
+export default Modal;
